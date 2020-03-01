db8752748ae90d5db8129bf7f8e663c570f4090e
Ryan Smith <Franzferdinan51@gmail.com>
2020-03-01 17:08:24 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -16,45 +16,7 @@ function App() {
   const [count, startCount] = useState(0);
   
 
-  // class timer extends Component {
-  //   constructor (props) {
-  //     super(props)
-  //     this.state = {
-  //       count: 1
-  //     }
-  //   }
-  
-  //   render () {
-  //     const {count} = this.state
-  //     return (
-  //       <div>
-  //         <h1>Current Count: {count}</h1>
-  //       </div>
-  //     )
-  //   }
-  //   // setInterval
-  //   // clearInterval
-  //   componentDidMount () {
-  //     const {startCount} = this.props
-  //     this.setState({
-  //       count: startCount
-  //     })
-  //     this.doIntervalChange()
-  //   }
-  
-  //   doIntervalChange = () => {
-  //       this.myInterval = setInterval(() => {
-  //       this.setState(prevState => ({
-  //         count: prevState.count - 1
-  //       }))
-  //     }, 1000)
-  //   }
-  
-  //   componentWillUnmount () {
-  //     clearInterval(this.myInterval)
-  //   }
-  // }
-  
+
   
   
   
